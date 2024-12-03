05c2366ec7fe2720bf2ef48e727ea63afc33cab1
Ivan Iossifov <iossifov@cshl.edu>
2024-12-03 12:04:49 -0500

    check point

--- python_access_demo.py
+++ python_access_demo.py
@@ -4,34 +4,22 @@ from dae.genomic_resources.reference_genome import build_reference_genome_from_r
 from dae.genomic_resources.genomic_scores import build_score_from_resource_id
 from dae.genomic_resources.gene_models import build_gene_models_from_resource_id
 
+chrom, pos_beg, pos_end = ("chr16", 30108313, 30108333)
 
+print("SEQUENCE")
+ref = build_reference_genome_from_resource_id("hg38/genomes/GRCh38.p14").open()
+print(ref.get_sequence(chrom, pos_beg, pos_end))
 
-###################
-#### TO REMOVE ####
-###################
-from dae.genomic_resources.genomic_scores import PositionScore
+print("\nTRANSCRIPTS")
+gm = build_gene_models_from_resource_id("hg38/gene_models/MANE/1.3").load()
+for tm in gm.gene_models_by_location(chrom, pos_beg, pos_end):
+    print(tm.tr_id, tm.gene, tm.is_coding(), tm.cds_len())
 
-def get_region_score(ps: PositionScore, chrom: str, beg: int, end: int, score_id: str):
-    r = [None] * (end - beg + 1)
-    for b, e, (v) in ps.fetch_region(chrom, beg, end, [score_id]):
-        if e > end:
-            e = end
-        r[b-beg:e-beg+1] = v
-    return r
-###################
-###################
-###################
+print("\nSCORE")
+ps = build_score_from_resource_id("hg38/scores/phyloP100way").open()
+print(ps.get_region_scores(chrom, pos_beg, pos_end, "phyloP100way"))
 
-chrom, pos_beg, pos_end = ("chr16", 30108313, 30108420)
-
-# ref = build_reference_genome_from_resource_id("hg38/genomes/GRCh38.p14").open()
-# print("The sequence is:", ref.get_sequence(chrom, pos_beg, pos_end))
-
-# gm = build_gene_models_from_resource_id("hg38/gene_models/GENCODE/46/comprehensive/ALL").load()
-# for tm in gm.gene_models_by_location(chrom, pos_beg, pos_end):
-#     print(tm.tr_id, tm.gene, tm.is_coding(), tm.cds_len())
-
-# ps = build_score_from_resource_id("hg38/scores/phyloP100way").open()
-# print(get_region_score(ps, chrom, pos_beg, pos_end, "phyloP100way"))
-
-# ass = build_score_from_resource_id("hg38/variant_frequencies/gnomAD_4.1.0/exomes/ALL").open()
+print("\nVARIANTS")
+ass = build_score_from_resource_id("hg38/variant_frequencies/gnomAD_4.1.0/exomes/ALL").open()
+for chrom, pos, ref, alt, [score] in ass.fetch_region(chrom, pos_beg, pos_end, ["AF"]):
+    print(pos, ref, alt, score)
